3154d84c304846658c23b064d6a656092baa5f78
Fabian Diener <f46200@gmail.com>
2024-12-20 20:46:45 +0100

    Fix Jest tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -10,8 +10,8 @@ import App from '../App';
 import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {render, waitFor} from '@testing-library/react-native';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders correctly', async () => {
+  await waitFor(() => render(<App />))
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest"
+  },
+  transformIgnorePatterns: [],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/"
+  ]
 };
