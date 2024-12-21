c13587c5b68af7167a468cdadc96a8bd621ee0c1
Fabian Diener <f46200@gmail.com>
2024-12-21 09:16:14 +0100

    Add CSpell EsLint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
 	root: true,
 	extends: [
 		'@react-native',
+		'plugin:@cspell/recommended',
 		'prettier'
 	],
 	rules: {
 		indent: ['error', 'tab'],
-	},
+		'@cspell/spellchecker': ['error', {}]
+	}
 };
