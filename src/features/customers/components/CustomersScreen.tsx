018f6bd05d90f3d1fc52a108e732228753d8fa01
Fabian Diener <f46200@gmail.com>
2024-12-23 17:50:29 +0100

    Show customers on screen

--- src/features/customers/components/CustomersScreen.tsx
+++ src/features/customers/components/CustomersScreen.tsx
@@ -1,5 +1,22 @@
+import React from 'react';
+import {FlatList, View} from 'react-native';
+import { useAppSelector } from '../../data/context/store';
+import { selectCustomerList } from '../context/customersSelectors';
+import { Customer } from '../types';
+import { Text } from 'react-native-paper';
+
+const CustomerItem = ({customer}: {customer: Customer}) => {
+	return <View>
+		<Text>{customer.name}</Text>
+	</View>;
+}
+
+const CustomerList = () => {
+	const customerList = useAppSelector(selectCustomerList);
+
+	return <FlatList data={customerList} renderItem={({item}) => <CustomerItem customer={item} />} />
+}
+
 export const CustomersScreen = () => {
-	return (
-		null
-	);
+	return <View><CustomerList /></View>;
 };
---
+++ src/features/customers/context/customersSelectors.ts
@@ -0,0 +1,9 @@
+import {createSelector} from '@reduxjs/toolkit';
+import {RootState} from 'src/features/data/context/store';
+
+const selectCustomers = (state: RootState) => state.customers;
+
+export const selectCustomerList = createSelector(
+	[selectCustomers],
+	customers => customers.customerList,
+);
