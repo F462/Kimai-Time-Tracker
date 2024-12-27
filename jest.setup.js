7eaffde22829988b7e9ef41e20a34e6c606993c4
Fabian Diener <f46200@gmail.com>
2024-12-27 23:05:48 +0100

    Add package `react-native-encrypted-storage` to securely store API key

--- jest.setup.js
+++ jest.setup.js
@@ -16,3 +16,10 @@ jest.mock('react-i18next', () => ({
 jest.mock('react-native-localize', () => ({
 	findBestLanguageTag: jest.fn()
 }));
+
+jest.mock('react-native-encrypted-storage', () => ({
+	setItem: jest.fn(() => Promise.resolve()),
+	getItem: jest.fn(() => Promise.resolve('{ "foo": 1 }')),
+	removeItem: jest.fn(() => Promise.resolve()),
+	clear: jest.fn(() => Promise.resolve())
+}));
