b110cb292bcc4811c9ce37251fd89d43610e3533
Fabian Diener <f46200@gmail.com>
2024-12-27 22:01:52 +0100

    Add listener to set API key when app started

--- src/features/account/middleware/accountListeners.ts
+++ src/features/account/middleware/accountListeners.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 import {apiKeyReceived, axiosHeadersSet} from '../context/accountActions';
 import {AppStartListening} from 'src/features/data/context/store';
+import {REHYDRATE} from 'redux-persist';
+import {selectApiKey} from '../context/accountSelectors';
 
 export const setAxiosHeadersOnApiKeyReceived = (
 	startListening: AppStartListening
@@ -15,6 +17,25 @@ export const setAxiosHeadersOnApiKeyReceived = (
 	});
 };
 
+export const setAxiosHeadersOnAppStart = (
+	startListening: AppStartListening
+) => {
+	startListening({
+		type: REHYDRATE,
+		effect: async (_, listenerApi) => {
+			const apiKey = selectApiKey(listenerApi.getState());
+
+			if (apiKey === undefined) {
+				return;
+			}
+
+			axios.defaults.headers.common.Authorization = `Bearer ${apiKey}`;
+			listenerApi.dispatch(axiosHeadersSet());
+		}
+	});
+};
+
 export const startAccountListeners = (startListening: AppStartListening) => {
 	setAxiosHeadersOnApiKeyReceived(startListening);
+	setAxiosHeadersOnAppStart(startListening);
 };
