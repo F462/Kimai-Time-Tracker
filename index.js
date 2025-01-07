a36a0f6993c987ec84c638b30b485b890a646c69
Fabian Diener <f46200@gmail.com>
2025-01-07 21:21:19 +0100

    Add dayjs plugin to be able to handle durations

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+dayjs.extend(duration);
+
 AppRegistry.registerComponent(appName, () => App);
