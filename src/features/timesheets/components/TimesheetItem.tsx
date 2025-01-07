c7e1e1cec3aef1deb1ea5edf9aa28388c379efb5
Fabian Diener <f46200@gmail.com>
2025-01-07 21:40:01 +0100

    Use HH:mm format for timesheet list durations

--- src/features/timesheets/components/TimesheetItem.tsx
+++ src/features/timesheets/components/TimesheetItem.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
 import {Text} from 'react-native-paper';
 import {View} from 'react-native';
+import dayjs from 'dayjs';
 
 import {Timesheet} from '../types';
 
 export const TimesheetItem = ({timesheet}: {timesheet: Timesheet}) => {
+	const duration = useMemo(() => dayjs.duration((timesheet.duration ?? 0) * 1000).format('HH:mm'), [timesheet.duration]);
+
 	return (
 		<View>
-			<Text>{timesheet.end} - {timesheet.begin} ({Math.round((timesheet.duration ?? 0) / 3600 * 10) / 10}h)</Text>
+			<Text>{timesheet.end} - {timesheet.begin} ({duration})</Text>
 		</View>
 	);
 };
