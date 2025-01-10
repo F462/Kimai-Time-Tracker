2de03edfe4b23d297980f8a513f132963b15d930
Fabian Diener <f46200@gmail.com>
2025-01-10 21:29:23 +0100

    Add ability to select projects in project list

--- src/features/projects/components/ProjectsScreen.tsx
+++ src/features/projects/components/ProjectsScreen.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 
 import {selectProjectList, selectSelectedProjectId} from '../context/projectsSelectors';
+import {useAppDispatch, useAppSelector} from 'src/features/data/context/store';
 import {BaseScreen} from 'src/ui/BaseScreen';
 import {DividedList} from 'src/ui/DividedList';
 import {ListItem} from 'src/ui/ListItem';
 import {ListItemText} from 'src/ui/ListItemText';
 import {Project} from '../types';
-import {useAppSelector} from 'src/features/data/context/store';
+import {projectSelected} from '../context/projectsSlice';
 
 const ProjectItem = ({project, isSelected}: {project: Project; isSelected: boolean;}) => {
+	const dispatch = useAppDispatch();
+	const onProjectItemPress = useCallback(() => {
+		dispatch(projectSelected(project.id));
+	}, [dispatch, project.id]);
+
 	return (
-		<ListItem isSelected={isSelected}>
+		<ListItem isSelected={isSelected} onPress={onProjectItemPress} >
 			<ListItemText>{project.name}</ListItemText>
 		</ListItem>);
 };
