474752db122f980caed1e0c5448e4b234c53a026
Fabian Diener <f46200@gmail.com>
2025-01-10 19:42:06 +0100

    Show "Now" when no end time is set (i.e. running timesheet

--- src/features/timesheets/components/TimesheetItem.tsx
+++ src/features/timesheets/components/TimesheetItem.tsx
@@ -3,15 +3,18 @@ import React, {useMemo} from 'react';
 import {Text} from 'react-native-paper';
 import {View} from 'react-native';
 import dayjs from 'dayjs';
+import {useTranslation} from 'react-i18next';
 
 import {Timesheet} from '../types';
 
 export const TimesheetItem = ({timesheet}: {timesheet: Timesheet}) => {
+	const {t} = useTranslation();
+
 	const displayedDuration = useMemo(() => dayjs.duration(timesheet.duration ? timesheet.duration * 1000 : dayjs().diff(dayjs(timesheet.begin))).format('HH:mm'), [timesheet.begin, timesheet.duration]);
 
 	return (
 		<View>
-			<Text>{timesheet.begin} - {timesheet.end} ({displayedDuration})</Text>
+			<Text>{timesheet.begin} - {timesheet.end ?? t('now')} ({displayedDuration})</Text>
 		</View>
 	);
 };
