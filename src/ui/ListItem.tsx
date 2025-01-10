8914e4bf60f7e7188b7de7d6deb83ca0f50825bf
Fabian Diener <f46200@gmail.com>
2025-01-10 21:27:09 +0100

    Add possibility to press list items

--- src/ui/ListItem.tsx
+++ src/ui/ListItem.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import {StyleSheet, View} from 'react-native';
 import {Checkbox} from 'react-native-paper';
+import {StyleSheet} from 'react-native';
+
+import {PressableOpacity} from './PressableOpacity';
 
 const styles = StyleSheet.create({
 	container: {
@@ -11,14 +13,14 @@ const styles = StyleSheet.create({
 	}
 });
 
-export const ListItem = ({isSelected, children}: React.PropsWithChildren<{isSelected?: boolean}>) => {
+export const ListItem = ({isSelected, onPress, children}: React.PropsWithChildren<{isSelected?: boolean; onPress?: () => void;}>) => {
 	return (
-		<View style={styles.container}>
+		<PressableOpacity style={styles.container} onPress={onPress} disabled={onPress === undefined}>
 			{isSelected === undefined ? null
 				: (<Checkbox
 					status={isSelected ? 'checked' : 'unchecked'}
 				/>)}
 			{children}
-		</View>
+		</PressableOpacity>
 	);
 };
