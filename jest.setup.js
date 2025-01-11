4efdd624435213673bb85f61c3e01d8923426d1f
Fabian Diener <f46200@gmail.com>
2025-01-11 09:56:21 +0100

    Add package react-native-device-info to get app version

--- jest.setup.js
+++ jest.setup.js
@@ -36,3 +36,7 @@ jest.mock('react-native-bootsplash', () => {
 	};
 });
 
+// see https://github.com/react-native-device-info/react-native-device-info/issues/1101#issuecomment-1007340089
+import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
+jest.mock('react-native-device-info', () => mockRNDeviceInfo);
+
