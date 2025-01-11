331db17640a7ec6cededacd829c134f484f23262
Fabian Diener <f46200@gmail.com>
2025-01-11 14:19:20 +0100

    Run synchronization immediately after a timesheet has been manipulated

--- src/features/synchronization/middleware/synchronizationListeners.ts
+++ src/features/synchronization/middleware/synchronizationListeners.ts
@@ -1,13 +1,21 @@
+import {ListenerEffectAPI, isAnyOf} from '@reduxjs/toolkit';
 import axios, {AxiosResponse} from 'axios';
-import {isAnyOf} from '@reduxjs/toolkit';
 import path from 'path';
 
+import {
+	AppDispatch,
+	AppStartListening,
+	RootState
+} from 'src/features/data/context/store';
+import {
+	newTimesheetStarted,
+	timesheetStopped
+} from '../../activeTimesheet/context/activeTimesheetSlice';
 import {
 	selectIsTimesheetKnownToServer,
 	selectRemoteTimesheetId,
 	selectUnsyncedTimesheets
 } from 'src/features/timesheets/context/timesheetsSelectors';
-import {AppStartListening} from 'src/features/data/context/store';
 import {TimesheetFromApi} from 'src/features/timesheets/types';
 import {internetReachabilityChanged} from 'src/features/network/context/networkSlice';
 import {selectIsInternetReachable} from 'src/features/network/context/networkSelector';
@@ -17,6 +25,65 @@ import {userLoggedIn} from 'src/features/account/context/accountActions';
 
 const SYNC_INTERVAL_IN_MILLISECONDS = 3000;
 let syncInterval: ReturnType<typeof setInterval> | undefined;
+
+async function sync(listenerApi: ListenerEffectAPI<RootState, AppDispatch>) {
+	const serverUrl = selectServerUrl(listenerApi.getState());
+
+	if (serverUrl === undefined) {
+		return;
+	}
+
+	const timesheetsToSync = selectUnsyncedTimesheets(listenerApi.getState());
+
+	if (Object.keys(timesheetsToSync).length === 0) {
+		return;
+	}
+
+	for (const [timesheetId, timesheet] of Object.entries(timesheetsToSync)) {
+		try {
+			let response: AxiosResponse<TimesheetFromApi>;
+			if (selectIsTimesheetKnownToServer(timesheetId)(listenerApi.getState())) {
+				const remoteId = selectRemoteTimesheetId(timesheetId)(
+					listenerApi.getState()
+				);
+				response = await axios.patch(
+					path.join(serverUrl, 'api/timesheets', remoteId.toString()),
+					{
+						begin: timesheet.begin,
+						end: timesheet.end,
+						project: timesheet.project,
+						activity: timesheet.activity
+					}
+				);
+			} else {
+				response = await axios.post(path.join(serverUrl, 'api/timesheets'), {
+					begin: timesheet.begin,
+					end: timesheet.end,
+					project: timesheet.project,
+					activity: timesheet.activity
+				});
+			}
+
+			listenerApi.dispatch(
+				timesheetSynced({
+					localId: timesheetId,
+					remoteId: response.data.id
+				})
+			);
+		} catch (error: any) {
+			console.warn('Got error on axios request: ', error.toString());
+		}
+	}
+}
+
+const runSyncOnActions = (startListening: AppStartListening) => {
+	startListening({
+		matcher: isAnyOf(newTimesheetStarted, timesheetStopped),
+		effect: async (_, listenerApi) => {
+			sync(listenerApi).catch(console.error);
+		}
+	});
+};
 
 const syncNewTimesheetsToServer = (startListening: AppStartListening) => {
 	startListening({
@@ -35,69 +102,8 @@ const syncNewTimesheetsToServer = (startListening: AppStartListening) => {
 				return;
 			}
 
-			async function sync() {
-				const serverUrl = selectServerUrl(listenerApi.getState());
-
-				if (serverUrl === undefined) {
-					return;
-				}
-
-				const timesheetsToSync = selectUnsyncedTimesheets(
-					listenerApi.getState()
-				);
-
-				if (Object.keys(timesheetsToSync).length === 0) {
-					return;
-				}
-
-				for (const [timesheetId, timesheet] of Object.entries(
-					timesheetsToSync
-				)) {
-					try {
-						let response: AxiosResponse<TimesheetFromApi>;
-						if (
-							selectIsTimesheetKnownToServer(timesheetId)(
-								listenerApi.getState()
-							)
-						) {
-							const remoteId = selectRemoteTimesheetId(timesheetId)(
-								listenerApi.getState()
-							);
-							response = await axios.patch(
-								path.join(serverUrl, 'api/timesheets', remoteId.toString()),
-								{
-									begin: timesheet.begin,
-									end: timesheet.end,
-									project: timesheet.project,
-									activity: timesheet.activity
-								}
-							);
-						} else {
-							response = await axios.post(
-								path.join(serverUrl, 'api/timesheets'),
-								{
-									begin: timesheet.begin,
-									end: timesheet.end,
-									project: timesheet.project,
-									activity: timesheet.activity
-								}
-							);
-						}
-
-						listenerApi.dispatch(
-							timesheetSynced({
-								localId: timesheetId,
-								remoteId: response.data.id
-							})
-						);
-					} catch (error: any) {
-						console.warn('Got error on axios request: ', error.toString());
-					}
-				}
-			}
-
 			syncInterval = setInterval(() => {
-				sync().catch(console.warn);
+				sync(listenerApi).catch(console.warn);
 			}, SYNC_INTERVAL_IN_MILLISECONDS);
 		}
 	});
@@ -106,5 +112,6 @@ const syncNewTimesheetsToServer = (startListening: AppStartListening) => {
 export const startSynchronizationListeners = (
 	startListening: AppStartListening
 ) => {
+	runSyncOnActions(startListening);
 	syncNewTimesheetsToServer(startListening);
 };
