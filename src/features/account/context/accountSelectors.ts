a706681ea208d7bf2db2ccebd89078af3f80c703
Fabian Diener <f46200@gmail.com>
2025-01-11 13:47:02 +0100

    Reword variables to better reflect their meaning

--- src/features/account/context/accountSelectors.ts
+++ src/features/account/context/accountSelectors.ts
@@ -2,9 +2,9 @@ import {createSelector} from '@reduxjs/toolkit';
 
 import {RootState} from 'src/features/data/context/store';
 
-const selectAccount = (state: RootState) => state.account;
+const selectAccountState = (state: RootState) => state.account;
 
 export const selectServerUrl = createSelector(
-	[selectAccount],
-	account => account.serverUrl
+	[selectAccountState],
+	accountState => accountState.serverUrl
 );
