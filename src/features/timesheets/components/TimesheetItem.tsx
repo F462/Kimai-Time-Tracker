f1aa9d7a8548ebe05668ed0bf07c7cdff8aa90c8
Fabian Diener <f46200@gmail.com>
2025-01-15 20:24:43 +0100

    Update duration in timesheet list when on screen

--- src/features/timesheets/components/TimesheetItem.tsx
+++ src/features/timesheets/components/TimesheetItem.tsx
@@ -7,6 +7,7 @@ import {useTranslation} from 'react-i18next';
 import {ListItem} from 'src/ui/ListItem';
 import {ListItemText} from 'src/ui/ListItemText';
 import {Timesheet} from '../types';
+import {useTime} from 'src/features/utils/useTime';
 
 const styles = StyleSheet.create({
 	datetimeText: {
@@ -20,7 +21,9 @@ const useDisplayedDatetime = (timestamp: string | null | undefined) => useMemo((
 export const TimesheetItem = ({timesheet}: {timesheet: Timesheet}) => {
 	const {t} = useTranslation();
 
-	const displayedDuration = useMemo(() => dayjs.duration(timesheet.duration ? timesheet.duration * 1000 : dayjs().diff(dayjs(timesheet.begin))).format('HH:mm'), [timesheet.begin, timesheet.duration]);
+	const now = useTime(20_000);
+
+	const displayedDuration = useMemo(() => dayjs.duration(timesheet.duration ? timesheet.duration * 1000 : now.diff(dayjs(timesheet.begin))).format('HH:mm'), [now, timesheet.begin, timesheet.duration]);
 	const displayedTimeStart = useDisplayedDatetime(timesheet.begin);
 	const displayedTimeEnd = useDisplayedDatetime(timesheet.end);
 
