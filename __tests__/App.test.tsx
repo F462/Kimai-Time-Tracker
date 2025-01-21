ebc3601e7b59556596938a1a54b18bde88e0df16
Fabian Diener <f46200@gmail.com>
2025-01-21 21:37:32 +0100

    Upgrade to RN 0.77.0 according to upgrade helper

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,17 +2,12 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
-
+import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import {render, waitFor} from '@testing-library/react-native';
-
-it('renders correctly', async () => {
-	await waitFor(() => render(<App />));
+test('renders correctly', async () => {
+	await ReactTestRenderer.act(() => {
+		ReactTestRenderer.create(<App />);
+	});
 });
