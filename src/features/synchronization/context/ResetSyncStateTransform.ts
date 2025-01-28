b1d6f86d7b158d7b1f7aeddb13ffe85ce518182b
Fabian Diener <f46200@gmail.com>
2025-01-28 19:59:05 +0100

    Mark running syncs as not started when app is started

---
+++ src/features/synchronization/context/ResetSyncStateTransform.ts
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import {createTransform} from 'redux-persist';
+
+import {SyncState, SynchronizationState} from '../types';
+
+export const ResetSyncStateTransform = createTransform(
+	undefined,
+	// transform state being rehydrated
+	(outboundState: SynchronizationState) => {
+		// convert mySet back to a Set.
+		return _.mapValues(outboundState, state =>
+			_.mapValues(state, syncState => {
+				if (syncState === SyncState.RUNNING) {
+					return SyncState.NOT_STARTED;
+				} else {
+					return syncState;
+				}
+			})
+		);
+	},
+	// define which reducers this transform gets called for.
+	{whitelist: ['synchronization']}
+);
