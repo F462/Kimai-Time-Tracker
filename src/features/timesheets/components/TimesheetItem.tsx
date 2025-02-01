18551e9018f254e9edaed2a05f052fe7af0a8f7c
Fabian Diener <f46200@gmail.com>
2025-02-01 13:52:58 +0100

    Add sync state indication of timesheet

--- src/features/timesheets/components/TimesheetItem.tsx
+++ src/features/timesheets/components/TimesheetItem.tsx
@@ -4,9 +4,13 @@ import {StyleSheet, View} from 'react-native';
 import dayjs from 'dayjs';
 import {useTranslation} from 'react-i18next';
 
+import {Icon} from 'react-native-paper';
 import {ListItem} from 'src/ui/ListItem';
 import {ListItemText} from 'src/ui/ListItemText';
+import {SyncState} from 'src/features/synchronization/types';
 import {Timesheet} from '../types';
+import {selectSyncState} from 'src/features/synchronization/context/synchronizationSelectors';
+import {useAppSelector} from 'src/features/data/context/store';
 import {useTime} from 'src/features/utils/useTime';
 
 const styles = StyleSheet.create({
@@ -18,10 +22,29 @@ const styles = StyleSheet.create({
 
 const useDisplayedDatetime = (timestamp: string | null | undefined) => useMemo(() => timestamp != null ? dayjs(timestamp).format('L LT') : undefined, [timestamp]);
 
+const SynchronizationStateIcon = ({synchronizationState}: {synchronizationState?: SyncState}) => {
+	const iconSource = (() => {
+		switch (synchronizationState) {
+		case SyncState.NOT_STARTED:
+			return 'timer-sand';
+		case SyncState.RUNNING:
+			return 'sync';
+		case SyncState.FAILED:
+			return 'exclamation';
+		case SyncState.DONE:
+		case undefined:
+			return 'check';
+		}
+	})();
+
+	return <Icon source={iconSource} size={15} />;
+};
+
 export const TimesheetItem = ({timesheet}: {timesheet: Timesheet}) => {
 	const {t} = useTranslation();
 
 	const now = useTime(20_000);
+	const synchronizationState = useAppSelector(selectSyncState(timesheet.id));
 
 	const displayedDuration = useMemo(() => {
 		const duration = (() => {
@@ -46,6 +69,9 @@ export const TimesheetItem = ({timesheet}: {timesheet: Timesheet}) => {
 
 	return (
 		<ListItem>
+			<View>
+				<SynchronizationStateIcon synchronizationState={synchronizationState} />
+			</View>
 			<ListItemText style={styles.datetimeText}>{displayedTimeStart}</ListItemText>
 			<ListItemText> - </ListItemText>
 			<ListItemText style={styles.datetimeText}>{displayedTimeEnd ?? t('now')}</ListItemText>
