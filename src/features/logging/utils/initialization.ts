61232ebc9866b60ccd9de21718f4a4f4fd6a853f
Fabian Diener <f46200@gmail.com>
2025-02-04 21:27:06 +0100

    Add basic integration of logging

---
+++ src/features/logging/utils/initialization.ts
@@ -0,0 +1,16 @@
+import {FileLogger} from 'react-native-file-logger';
+import ReactNativeBlobUtil from 'react-native-blob-util';
+import path from 'path';
+
+export const LOGS_DIRECTORY = path.join(
+	ReactNativeBlobUtil.fs.dirs.CacheDir,
+	'logs'
+);
+
+FileLogger.configure({
+	logsDirectory: LOGS_DIRECTORY
+})
+	.then(() => {
+		FileLogger.info('File logger initialized');
+	})
+	.catch(console.error);
