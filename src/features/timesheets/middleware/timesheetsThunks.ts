d7a6ea6e3dbafd20c257a54078f48d338b0ab4a7
Fabian Diener <f46200@gmail.com>
2025-03-08 21:46:57 +0100

    Rework fetching of online timesheets to be able to update local state for deleted timesheets on remote side

--- src/features/timesheets/middleware/timesheetsThunks.ts
+++ src/features/timesheets/middleware/timesheetsThunks.ts
@@ -1,11 +1,16 @@
 import {FileLogger} from 'react-native-file-logger';
 import axios from 'axios';
 import path from 'path';
+import {v4 as uuidv4} from 'uuid';
 
+import {
+	selectKnownRemoteTimesheetIds,
+	selectOnlyLocalTimesheets
+} from '../context/timesheetsSelectors';
 import {TimesheetFromApi} from '../types';
 import {createAppAsyncThunk} from 'src/features/data/middleware/createAppAsyncThunk';
 import {selectServerUrl} from 'src/features/account/context/accountSelectors';
-import {timesheetsReceived} from '../context/timesheetsSlice';
+import {timesheetsUpdated} from '../context/timesheetsSlice';
 
 export const fetchTimesheets = createAppAsyncThunk(
 	'timesheets/fetchTimesheets',
@@ -20,7 +25,26 @@ export const fetchTimesheets = createAppAsyncThunk(
 			const response = await axios.get<Array<TimesheetFromApi>>(
 				path.join(serverUrl, 'api/timesheets')
 			);
-			dispatch(timesheetsReceived(response.data));
+
+			const knownRemoteTimesheetIds = selectKnownRemoteTimesheetIds(getState());
+
+			const onlyLocalTimesheets = selectOnlyLocalTimesheets(getState());
+
+			const allTimesheets = response.data.reduce((container, element) => {
+				const id = (() => {
+					const idInKnownRemoteTimesheetTable =
+						knownRemoteTimesheetIds[element.id];
+					if (idInKnownRemoteTimesheetTable) {
+						return idInKnownRemoteTimesheetTable;
+					}
+
+					return uuidv4();
+				})();
+
+				return {...container, [id]: {...element, id}};
+			}, onlyLocalTimesheets);
+
+			dispatch(timesheetsUpdated(allTimesheets));
 		} catch (error: any) {
 			FileLogger.warn(`Got error on axios request: ${error.toString()}`);
 		}
