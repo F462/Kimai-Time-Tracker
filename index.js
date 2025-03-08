5edc4388143c3c53352d5c1b5aecec4ed777f6d0
Fabian Diener <f46200@gmail.com>
2025-03-08 22:18:00 +0100

    Add parsing time format to dayjs to fix beginning time

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 dayjs.extend(duration);
+dayjs.extend(customParseFormat);
 
 import 'src/features/logging/utils/initialization';
 import 'src/features/debugging/utils/whyDidYouRender';
