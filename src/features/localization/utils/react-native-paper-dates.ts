f822366ae50b8cd6c88ec01bb91b56692d1c310f
Fabian Diener <f46200@gmail.com>
2025-08-07 22:05:46 +0200

    Replace react-native-dates for RN Datetime picker for better UX

--- src/features/localization/utils/react-native-paper-dates.ts
+++
@@ -1,4 +0,0 @@
-// see https://web-ridge.github.io/react-native-paper-dates/docs/intro#register-translation
-import {enGB, registerTranslation} from 'react-native-paper-dates';
-
-registerTranslation('en', enGB);
