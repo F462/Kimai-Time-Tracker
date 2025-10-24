f17d38caf8b8f563cd9144f3075a10d214c508d4
Fabian Diener <f46200@gmail.com>
2025-10-24 23:14:33 +0200

    Add rudimentary dayComponent for custom day rendering

--- src/features/contract/components/ContractSummaryScreen.tsx
+++ src/features/contract/components/ContractSummaryScreen.tsx
@@ -1,11 +1,39 @@
+import React, {useCallback} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {Calendar} from 'react-native-calendars';
-import React from 'react';
+import {Text} from 'react-native-paper';
 
 import {BaseScreen} from 'src/ui/BaseScreen';
 import {useCalendarTheme} from 'src/features/utils/useCalendarTheme';
 
+const styles = StyleSheet.create({
+	dayContainer: {
+		padding: 10,
+	},
+	dayText: {
+		alignSelf: 'flex-end',
+	},
+});
+
 export const ContractSummaryScreen = () => {
 	const calendarTheme = useCalendarTheme();
+
+	const dayRenderer = useCallback(
+		({
+			date,
+		}: React.ComponentProps<
+			NonNullable<React.ComponentProps<typeof Calendar>['dayComponent']>
+		>) => {
+			return (
+				<View style={styles.dayContainer}>
+					<Text style={styles.dayText}>{date?.day}</Text>
+					<Text style={styles.dayText}>+3</Text>
+				</View>
+			);
+		},
+		[],
+	);
+
 	return (
 		<BaseScreen>
 			<Calendar
@@ -15,6 +43,7 @@ export const ContractSummaryScreen = () => {
 				// If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
 				firstDay={1}
 				showWeekNumbers={true}
+				dayComponent={dayRenderer}
 			/>
 		</BaseScreen>
 	);
